5f3a23bf01e7acf9d3ad3743bb7d09746c5462d1
League Student <league@iMac-4.attlocal.net>
2019-02-13 18:26:31 -0800

    v
    v

---
+++ src/wierd.java
@@ -0,0 +1,15 @@
+import org.jointheleague.graphical.robot.Robot;
+
+public class wierd {
+public static void main(String[] args)  {
+	Robot r = new Robot("batman");
+	r.penDown();
+	r.setX(100);
+	r.setY(100);
+	r.move(100);
+	r.turn(90);
+
+
+
+}
+}
